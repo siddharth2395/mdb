243ca56120e2622d679cb1cfe70a12e477576cfe
Bhavsar Siddharth <siddharth2395@users.noreply.github.com>
2017-08-15 15:43:43 +0530

    index.php for game selection

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="theme-color" content="black" />
     <title>Material Design Bootstrap</title>
     <!-- Font Awesome -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css">
@@ -21,17 +22,98 @@
     <!-- Start your project here-->
     <!--Main Navigation-->
     <header>
+        <nav class="navbar navbar-expand-lg navbar-dark black">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01"
+        aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
 
+    </button>
+            <a class="navbar-brand " href="#">Book Yo place</a>
+    <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+        <ul class="navbar-nav mr-auto mt-lg-0">
+            <li class="nav-item active">
+                <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Link</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Link</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Link</a>
+            </li>
+        </ul>
+    </div>
+</nav>
     </header>
 
     <!--Main layout-->
-    <main>
-        <h1><?php echo "hello"; ?></h1>
+    <main class="mt-1">
+        <div class="container">
+
+            <div class="row">
+                <div class="col-md-8 p-0 custom-card-dark text-center">
+                    <!--Dropdown primary-->
+                    <div class="h-50">
+                        <img src="img/LOGO.jpg" class="img-fluid" />
+                    </div>
+                    <span class="white-text ">Please select the game you want to play</span>
+
+                    <div class="dropdown ">
+                        <!--Trigger-->
+                        <button class="btn btn-danger dropdown-toggle waves-effect waves-light" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">select game</button>
+                        <!--Menu-->
+                        <div class="dropdown-menu">
+                            <a class="dropdown-item" href="#">Need for speed</a>
+                            <a class="dropdown-item" href="#">CSGO</a>
+                            <a class="dropdown-item" href="#">Phantom</a>
+                            <a class="dropdown-item" href="#">Novacore</a>
+                            <a class="dropdown-item" href="#">GTA-V</a>
+                        </div>
+                    </div>
+                    <!--/Dropdown primary-->
+                    <p class="white-text mt-5">*Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.*</p>
+                </div>
+            </div>
+        </div>
     </main>
 
     <!--Footer-->
     <footer>
+        <footer class="page-footer black center-on-small-only">
 
+    <!--Footer Links-->
+    <div class="container-fluid">
+        <div class="row">
+
+            <!--First column-->
+            <div class="col-md-6">
+                <h5 class="title">Footer Content</h5>
+                <p>Here you can use rows and columns here to organize your footer content.</p>
+            </div>
+            <!--/.First column-->
+
+            <!--Second column-->
+            <div class="col-md-6">
+                <h5 class="title">Links</h5>
+                <ul>
+                    <li><a href="#!">Link 3</a></li>
+                    <li><a href="#!">Link 4</a></li>
+                </ul>
+            </div>
+            <!--/.Second column-->
+        </div>
+    </div>
+    <!--/.Footer Links-->
+
+    <!--Copyright-->
+    <div class="footer-copyright">
+        <div class="container-fluid">
+            © 2015 Copyright: <a href="https://www.MDBootstrap.com"> MDBootstrap.com </a>
+
+        </div>
+    </div>
     </footer>
     <!-- /Start your project here-->
 
